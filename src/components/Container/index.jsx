8363bae201bcca30ebef5b472791006991683e15
joykepler <joyjoykepler@gmail.com>
2024-07-04 14:18:28 -0300

    background

--- src/components/Container/index.jsx
+++ src/components/Container/index.jsx
@@ -5,10 +5,11 @@ import Button from '../Button'
 const Container = () => {
     return (
         <div className='container'>
-            <h1>Back to your digital life</h1>
-            <p>Choose one of the option to go</p>
-            <Input type="email" placeholder="example@gmail.com"/>
-            <Input type="password" placeholder="Password"/>
+                <h1>Login</h1>
+            <div>
+                <Input type="email" placeholder="example@gmail.com"/>
+                <Input type="password" placeholder="Password"/>
+            </div>
             <Button />
         </div>
     )
